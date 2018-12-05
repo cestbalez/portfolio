6d15836016c324a6bc547d3ec7542bb3c56543cc
Magnus Remoe <mremoe@gmail.com>
2018-12-05 16:17:59 +0800

    Fixed bug in JS for mobile view height

--- source/javascripts/window_sizing.js
+++ source/javascripts/window_sizing.js
@@ -1,4 +1,4 @@
-function windowSizing {
+function windowSizing() {
 
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
@@ -10,6 +10,6 @@ function windowSizing {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+  });
 
 }
